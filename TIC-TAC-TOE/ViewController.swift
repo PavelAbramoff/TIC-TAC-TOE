092122860973dea2a78209489363d5229c63accd
PavelAbramoff <Pavel.818@yandex.ru>
2024-09-29 11:16:04 -0700

    Initial Commit

---
+++ TIC-TAC-TOE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TIC-TAC-TOE
+//
+//  Created by apple on 9/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TIC-TAC-TOETests/TIC_TAC_TOETests.swift
@@ -0,0 +1,17 @@
+//
+//  TIC_TAC_TOETests.swift
+//  TIC-TAC-TOETests
+//
+//  Created by apple on 9/29/24.
+//
+
+import Testing
+@testable import TIC_TAC_TOE
+
+struct TIC_TAC_TOETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
