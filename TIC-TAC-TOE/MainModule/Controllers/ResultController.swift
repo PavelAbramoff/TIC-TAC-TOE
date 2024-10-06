b6ab8ca0120648904bb8ea62468eb221224fd2e8
AryaDarisova <arisova.darya@yandex.ru>
2024-10-06 10:54:56 +0300

    Revert "Add all other routes and change UI for stack view in GameScreen1"
    This reverts commit e28e7a86bfebd1241e5027ac04c6ed778c14ec4d.

--- TIC-TAC-TOE/MainModule/Controllers/ResultController.swift
+++ TIC-TAC-TOE/MainModule/Controllers/ResultController.swift
@@ -99,10 +99,6 @@ class ResultController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
             self.backBtn.backgroundColor = .blue
         }
-        
-        let vc = GameScreen1ViewController()
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true, completion: nil)
     }
     
     @objc func btnBackTapped() {
