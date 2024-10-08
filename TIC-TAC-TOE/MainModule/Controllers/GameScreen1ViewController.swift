a19580577f0692c4e0b7b4e2e2e64577470cc471
PavelAbramoff <Pavel.818@yandex.ru>
2024-10-07 21:56:43 -0700

    test

--- TIC-TAC-TOE/MainModule/Controllers/GameScreen1ViewController.swift
+++ TIC-TAC-TOE/MainModule/Controllers/GameScreen1ViewController.swift
@@ -142,6 +142,177 @@ final class GameScreen1ViewController: UIViewController {
         view.addSubview(squareStackView)
     }
 }
+
+// MARK: Logic Game
+extension GameScreen1ViewController {
+    @objc func buttonTapped(sender: UIButton) {
+        let index = sender.tag
+        
+        // Проверяем, что клетка пуста
+        if board[index] == 0 {
+            // Заполняем клетку в зависимости от текущего игрока
+            if currentPlayer == 1 {
+                sender.setBackgroundImage(UIImage(named: "Xskin1"), for: .normal)
+                board[index] = 1
+                updateTurnIndicator(numberOfUser: currentPlayer)
+            } else {
+                sender.setBackgroundImage(UIImage(named: "Oskin1"), for: .normal)
+                board[index] = 2
+                updateTurnIndicator(numberOfUser: currentPlayer)
+            }
+            
+            // Проверяем, есть ли победитель
+            if let winner = checkForWinner() {
+                showWinner(winner: winner)
+            } else if board.allSatisfy({ $0 != 0 }) {
+                // Если все клетки заполнены, и нет победителя — ничья
+                showWinner(winner: 0)
+            }
+            
+            // Переключаем игрока
+            currentPlayer = currentPlayer == 1 ? 2 : 1
+        }
+        var printString: String = " "
+        for i in board {
+            
+            printString += String(i)
+            
+        }
+        print(printString)
+    }
+    
+    // Проверяем, есть ли победитель
+    private func checkForWinner() -> Int? {
+        for combination in winningCombinations {
+            let (a, b, c) = (board[combination[0]], board[combination[1]], board[combination[2]])
+            if a != 0 && a == b && b == c {
+                return a // Возвращаем победителя (1 - крестик, 2 - нолик)
+            }
+        }
+        return nil
+    }
+    
+    // MARK: - WINNER
+    private func showWinner(winner: Int) {
+        // 0 - Ничья, 1 - Победа крестики, 2 - Победа нолик
+        print("Победитель - \(winner)")
+        stopTimer()
+        
+        if let combination = winningCombinations.first(where: { checkCombination($0) == winner }) {
+            drawWinningLine(for: combination)
+            
+        }
+        // добавить логику перехода на экран результата
+        
+        //        let vc = ResultController()
+        //        vc.modalPresentationStyle = .fullScreen
+        //        present(vc, animated: true)
+    }
+    
+    // MARK: Add Winn Line
+    private func checkCombination(_ combination: [Int]) -> Int? {
+        let (a, b, c) = (board[combination[0]], board[combination[1]], board[combination[2]])
+        if a != 0 && a == b && b == c {
+            return a
+        }
+        return nil
+    }
+    
+    private func getCenterPoint(for index: Int) -> CGPoint {
+        // Расчет позиции ячейки на экране
+        let row = index / 3
+        let column = index % 3
+        
+        // Размеры ячейки
+        let buttonSize: CGFloat = 74 // Размер кнопки
+        let spacing: CGFloat = 20     // Промежуток между ячейками
+        
+        // Начальная точка
+        let startX = (CGFloat(column) * (buttonSize + spacing)) + (buttonSize / 2) + 70 // Центр ячейки по X
+        let startY = (CGFloat(row) * (buttonSize + spacing)) + (buttonSize / 2) + 380 // Центр ячейки по Y
+        
+        return CGPoint(x: startX, y: startY)
+    }
+    
+    private func drawWinningLine(for combination: [Int]) {
+        // Удаляем предыдущую линию, если она есть
+        winningLine?.removeFromSuperlayer()
+        
+        // Получаем координаты для линии
+        let startPoint = getCenterPoint(for: combination[0])
+        let endPoint = getCenterPoint(for: combination[2]) // используем конечную точку линии
+        
+        let path = UIBezierPath()
+        path.move(to: startPoint)
+        path.addLine(to: endPoint)
+        
+        winningLine = CAShapeLayer()
+        winningLine?.path = path.cgPath
+        winningLine?.strokeColor = UIColor.purple.cgColor
+        winningLine?.lineWidth = 14
+        winningLine?.fillColor = UIColor.clear.cgColor
+        
+        view.layer.addSublayer(winningLine!)
+    }
+    
+    // Сбрасываем игру
+    private func resetGame() {
+        board = [Int](repeating: 0, count: 9)
+        currentPlayer = 1
+        stopTimer()
+        
+        // Получаем все кнопки из squareStackView
+        for subStack in squareStackView.arrangedSubviews {
+            if let rowStack = subStack as? UIStackView {
+                for subview in rowStack.arrangedSubviews {
+                    if let button = subview as? UIButton {
+                        button.setBackgroundImage(.cell, for: .normal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Timer
+extension GameScreen1ViewController {
+    private func getTimeDuration() {
+        guard let gameTimeDuration = UserDefaults.standard.object(forKey: "GameTimeDuration") as? Int else {
+            totalSeconds = 30
+            return
+        }
+        if gameTimeDuration > 0 {
+            totalSeconds = gameTimeDuration
+        }
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTimer() {
+        if totalSeconds > 0 {
+            totalSeconds -= 1
+            timerLabel.text = formatTime(seconds: totalSeconds)
+            
+        } else {
+            stopTimer()
+            // Сделать переход на проигрыш из-за истечения времени
+        }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate() // Остановка таймера по истечению времени
+        timer = nil
+    }
+    
+    private func formatTime(seconds: Int) -> String {
+        let minutes = seconds / 60
+        let remainingSeconds = seconds % 60
+        return String(format: "%02d:%02d", minutes, remainingSeconds)
+    }
+}
+
 // MARK: - Setup Constraints
 private extension GameScreen1ViewController {
      func setupConstraints() {
